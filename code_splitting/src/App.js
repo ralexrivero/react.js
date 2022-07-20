a54f076f9571649f0006a9d61be7ac8a186f9fa9
ralexrivero <ralexrivero@gmail.com>
2022-07-20 00:58:24 -0300

    minor fix

--- code_splitting/src/App.js
+++ code_splitting/src/App.js
@@ -1,7 +1,6 @@
 import React, {Component} from 'react';
 import './App.css';
 import Page1 from './components/Page1';
-import Page2 from './components/Page2';
 
 class App extends Component {
   constructor() {
