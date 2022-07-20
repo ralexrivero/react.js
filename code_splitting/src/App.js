ebfcf72414068672105ae1b50ea3a95d5d810a83
ralexrivero <ralexrivero@gmail.com>
2022-07-19 22:41:01 -0300

    initial commit

---
+++ code_splitting/src/App.js
@@ -0,0 +1,17 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+      </header>
+      <Page_1 />
+      <Page_2 />
+      <Page_3 />
+    </div>
+  );
+}
+
+export default App;
