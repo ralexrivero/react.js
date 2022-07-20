ce1e98867dd119ada28cbbc6475300be2d9910b4
ralexrivero <ralexrivero@gmail.com>
2022-07-19 23:34:22 -0300

    add disabled button and style

--- code_splitting/src/components/Page1.js
+++ code_splitting/src/components/Page1.js
@@ -7,6 +7,7 @@ const Page1 = ({onRouteChange}) =>
             <img src={logo} className="App-logo" alt="logo" />
             <h1>Welcome to page 1</h1>
         </header>
+        <button className='disabled'>Page1</button>
         <button onClick={() => onRouteChange('page2')}>Page2</button>
         <button onClick={() => onRouteChange('page3')}>Page3</button>
     </div>
--- code_splitting/src/components/Page2.js
+++ code_splitting/src/components/Page2.js
@@ -8,6 +8,7 @@ const Page2 = ({onRouteChange}) =>
             <h1>Welcome to page 2</h1>
         </header>
         <button onClick={() => onRouteChange('page1')}>Page1</button>
+        <button className='disabled'>Page2</button>
         <button onClick={() => onRouteChange('page3')}>Page3</button>
     </div>
 
