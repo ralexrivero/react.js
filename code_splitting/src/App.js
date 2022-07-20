f75e896d5d37ce3b75db4e6aebf252f59879889e
ralexrivero <ralexrivero@gmail.com>
2022-07-19 23:30:05 -0300

    add routing

--- code_splitting/src/App.js
+++ code_splitting/src/App.js
@@ -1,17 +1,31 @@
+import React, {Component} from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Page1 from './components/Page1';
+import Page2 from './components/Page2';
+import Page3 from './components/Page3';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-      </header>
-      <Page_1 />
-      <Page_2 />
-      <Page_3 />
-    </div>
-  );
+class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      route: 'page1'
+    }
+  }
+
+  onRouteChange = (route) => {
+    this.setState({route: route});
+  }
+
+  render () {
+    if (this.state.route === 'page1') {
+      return <Page1 onRouteChange={this.onRouteChange} />
+    }
+    else if (this.state.route === 'page2') {
+      return <Page2 onRouteChange={this.onRouteChange}/>
+    }
+    else
+    return <Page3 onRouteChange={this.onRouteChange}/>
+  }
 }
-
 export default App;
