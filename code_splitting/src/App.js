62eda3965ad6823e387833a0fa81055f36ae8026
ralexrivero <ralexrivero@gmail.com>
2022-07-21 21:56:03 -0300

    update to React.lazy

--- code_splitting/src/App.js
+++ code_splitting/src/App.js
@@ -1,7 +1,10 @@
-import React, {Component} from 'react';
+import React, {Component, Suspense} from 'react';
 import './App.css';
 import Page1 from './components/Page1';
-import AsyncComponent from './components/AsyncComponent';
+// import AsyncComponent from './components/AsyncComponent';
+const Page2Lazy = React.lazy(() => import('./components/Page2'));
+const Page3Lazy = React.lazy(() => import('./components/Page3'));
+
 
 class App extends Component {
   constructor() {
@@ -28,14 +31,30 @@ class App extends Component {
   }
 
   render () {
+    // if (this.state.route === 'page1') {
+    //   return <Page1 onRouteChange={this.onRouteChange} />
+    // } else if (this.state.route === 'page2') {
+    //   const AsyncPage2 = AsyncComponent(()=> import('./components/Page2'));
+    //   return <AsyncPage2 onRouteChange={this.onRouteChange} />
+    // } else if (this.state.route === 'page3') {
+    //   const AsyncPage3 = AsyncComponent(()=> import('./components/Page3'));
+    //   return <AsyncPage3 onRouteChange={this.onRouteChange} />
+    // }
+
     if (this.state.route === 'page1') {
       return <Page1 onRouteChange={this.onRouteChange} />
     } else if (this.state.route === 'page2') {
-      const AsyncPage2 = AsyncComponent(()=> import('./components/Page2'));
-      return <AsyncPage2 onRouteChange={this.onRouteChange} />
+      return (
+        <Suspense fallback={<div>Loading...</div>}>
+          <Page2Lazy onRouteChange={this.onRouteChange} />
+        </Suspense>
+      );
     } else if (this.state.route === 'page3') {
-      const AsyncPage3 = AsyncComponent(()=> import('./components/Page3'));
-      return <AsyncPage3 onRouteChange={this.onRouteChange} />
+      return (
+        <Suspense fallback={<div>Loading...</div>}>
+          <Page3Lazy onRouteChange={this.onRouteChange} />
+        </Suspense>
+      );
     }
   }
 }
--- code_splitting/src/components/AsyncComponent.js
+++ code_splitting/src/components/AsyncComponent.js
@@ -1,4 +1,4 @@
-import React, {Component} from 'react';
+/* import React, {Component} from 'react';
 
 export default function asyncComponent(importComponent) {
     class AsyncComponent extends Component {
@@ -23,4 +23,4 @@ export default function asyncComponent(importComponent) {
     }
 
     return AsyncComponent;
-}
+} */
