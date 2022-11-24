f482c60538fb089a5c332950d90463c6675f53b5
ralexrivero <ralexrivero@gmail.com>
2022-11-24 17:39:53 -0300

    use state hook

--- 01-Create_React_App/my-app/src/App.js
+++ 01-Create_React_App/my-app/src/App.js
@@ -1,22 +1,15 @@
-import logo from './logo.svg';
 import './App.css';
+import { useState } from "react";
 
 function App() {
+  const [emotion, setEmotion] = useState("happy");
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>Current emotion in {emotion}</h1>
+        <button onClick={() => setEmotion("sad")}>Sad</button>
+        <button onClick={() => setEmotion("excited")}>Excited</button>
       </header>
     </div>
   );
