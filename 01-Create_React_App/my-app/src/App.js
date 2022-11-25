f0e18e3d2212ed69053124f7b2f2b385dbe9bc51
ralexrivero <ralexrivero@gmail.com>
2022-11-24 23:57:43 -0300

    useState in check

--- 01-Create_React_App/my-app/src/App.js
+++ 01-Create_React_App/my-app/src/App.js
@@ -1,26 +1,20 @@
 import './App.css';
-import { useState, useEffect } from "react";
+import { useState } from "react";
 
 function App() {
-  const [emotion, setEmotion] = useState("happy");
-  const [secondary, setSecondary] = useState("tired");
-
-  useEffect(() => {
-    console.log(`It's ${emotion} right now`);
-  }, [emotion, secondary]);
-
-  useEffect(() => {
-    console.log(`It's ${secondary} right now`);
-  }, [secondary]);
+  const [checked, setChecked] = useState(false);
   return (
     <div className="App">
-      <header className="App-header">
-        <h1>Current emotion in {emotion}</h1>
-        <button onClick={() => setEmotion("sad")}>Sad</button>
-        <button onClick={() => setEmotion("excited")}>Excited</button>
-        <h2>Current secondary emotion is {secondary}.</h2>
-        <button onClick={() => setSecondary("grateful")}>Grateful</button>
-      </header>
+      <input
+        type="checkbox"
+        value={checked}
+        onChange={() =>
+          setChecked((checked) => !checked)
+          }
+      />
+      <label>
+        {checked ? "checked" : "not checked"}
+       </label>
     </div>
   );
 }
---
+++ 01-Create_React_App/my-app/src/app_versions/00-App.js
@@ -0,0 +1,28 @@
+import './App.css';
+import { useState, useEffect } from "react";
+
+function App() {
+  const [emotion, setEmotion] = useState("happy");
+  const [secondary, setSecondary] = useState("tired");
+
+  useEffect(() => {
+    console.log(`It's ${emotion} right now`);
+  }, [emotion, secondary]);
+
+  useEffect(() => {
+    console.log(`It's ${secondary} right now`);
+  }, [secondary]);
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>Current emotion in {emotion}</h1>
+        <button onClick={() => setEmotion("sad")}>Sad</button>
+        <button onClick={() => setEmotion("excited")}>Excited</button>
+        <h2>Current secondary emotion is {secondary}.</h2>
+        <button onClick={() => setSecondary("grateful")}>Grateful</button>
+      </header>
+    </div>
+  );
+}
+
+export default App;
