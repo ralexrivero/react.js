6c3a0d5623a1d324132c91670cda9d90b5784e71
ralexrivero <ralexrivero@gmail.com>
2022-11-26 00:29:22 -0300

    use graphql

--- 01-Create_React_App/my-app/src/App.js
+++ 01-Create_React_App/my-app/src/App.js
@@ -1,12 +1,27 @@
 import './App.css';
 import { useState, useEffect } from 'react';
 
-function GithubUser({ name, location, avatar }) {
+const query = `
+query {
+  allLifts {
+    name
+    elevationGain
+    status
+  }
+}
+`;
+
+const opts = {
+  method: 'POST',
+  headers: {'Content-Type': 'application/json'},
+  body: JSON.stringify({query})
+}
+
+function Lift({ name, elevationGain, status }) {
   return (
     <div>
       <h1>{name}</h1>
-      <p>{location}</p>
-      <img src={avatar} height={150} alt={name} />
+      <p>{elevationGain} {status}</p>
     </div>
   );
 }
@@ -19,7 +34,8 @@ function App () {
   useEffect(() => {
     setLoading(true);
     fetch(
-      `https://api.github.com/users/ralexrivero`
+      `http://snowtooth.moonhighway.com/`,
+      opts
     ).then((response) => response.json())
     .then(setData)
     .then(() => setLoading(false))
@@ -32,11 +48,13 @@ if (error)
 if (!data) return null;
 return (
   <div className='App'>
-    <GithubUser
-      name={data.name}
-      location={data.location}
-      avatar={data.avatar_url}
-    />
+    {data.data.allLifts.map((lift) => (
+      <Lift
+        name={lift.name}
+        elevationGain={lift.elevationGain}
+        status={lift.status}
+      />
+    ))}
   </div>
 );
 }
