9ca0a9a72c6e13357fb3f4c4bf093859811281a3
ralexrivero <ralexrivero@gmail.com>
2022-11-26 01:54:19 -0300

    add child page and nested link with react router

--- 01-Create_React_App/my-app/src/index.js
+++ 01-Create_React_App/my-app/src/index.js
@@ -1,14 +1,16 @@
 import React from 'react';
 import ReactDom from 'react-dom';
 import './index.css';
-import { App, About, Contact } from './App';
+import { App, About, Contact, History } from './App';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
 ReactDom.render(
   <BrowserRouter>
     <Routes>
       <Route path='/' element={<App />} />
-      <Route path='/about' element={<About />} />
+      <Route path='/about' element={<About />}>
+        <Route path='history' element={<History />} />
+      </Route>
       <Route path='/contact' element={<Contact />} />
     </Routes>
   </BrowserRouter>, document.getElementById('root')
