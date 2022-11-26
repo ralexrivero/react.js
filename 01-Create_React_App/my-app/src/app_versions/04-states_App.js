acdefa20843e694fadc7abd65d6f261b44e13a26
ralexrivero <ralexrivero@gmail.com>
2022-11-25 23:49:49 -0300

    loading states

---
+++ 01-Create_React_App/my-app/src/app_versions/04-states_App.js
@@ -0,0 +1,44 @@
+import './App.css';
+import { useState, useEffect } from 'react';
+
+function GithubUser({ name, location, avatar }) {
+  return (
+    <div>
+      <h1>{name}</h1>
+      <p>{location}</p>
+      <img src={avatar} height={150} alt={name} />
+    </div>
+  );
+}
+
+function App () {
+  const [data, setData] = useState(null);
+  const [error, setError] = useState(null);
+  const [loading, setLoading] = useState(false);
+
+  useEffect(() => {
+    setLoading(true);
+    fetch(
+      `https://api.github.com/users/ralexrivero`
+    ).then((response) => response.json())
+    .then(setData)
+    .then(() => setLoading(false))
+    .catch(setError);
+  }, []);
+
+if (loading) return <h1>Loading...</h1>;
+if (error)
+  return <pre>{JSON.stringify(error)}</pre>;
+if (!data) return null;
+return (
+  <div className='App'>
+    <GithubUser
+      name={data.name}
+      location={data.location}
+      avatar={data.avatar_url}
+    />
+  </div>
+);
+}
+
+export default App;
