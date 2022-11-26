018460e3493793b13bec2dff700a80bc632916e1
ralexrivero <ralexrivero@gmail.com>
2022-11-26 01:15:20 -0300

    install react-router-dom and create a few functions

--- 01-Create_React_App/my-app/src/App.js
+++ 01-Create_React_App/my-app/src/App.js
@@ -1,38 +1,33 @@
 import './App.css';
 
-const tahoe_peaks = [
-  { name: 'Freel', elevation: 10891 },
-  { name: 'Monument, elevation>: 10067' },
-  { name: 'Pyramid', elevation: 9983 },
-  { name: 'Tallac', elevation: 9735 }
-];
+function Home() {
+  return (
+    <div>
+      <h1>Home page</h1>
+    </div>
+  )
+}
 
-function List({data, renderItem, renderEmpty }) {
-  return !data.length ? (
-    renderEmpty
-  ) : (
-    <ul>
-      {data.map((item) => (
-        <li key={item.name}>
-          {renderItem(item)}
-        </li>
-      ))}
-    </ul>
-  );
+function About() {
+  return (
+    <div>
+      <h1>About us</h1>
+    </div>
+  )
+}
+
+function Contact() {
+  return (
+    <div>
+      <h1>Contact Us</h1>
+    </div>
+  )
 }
 
 function App() {
   return (
-    <List
-      data={tahoe_peaks}
-      renderEmpty={<p>This list is empty</p>}
-      renderItem={(item) => (
-        <>
-          {item.name} - {item.elevation} ft.
-        </>
-      )}
-    />
-  );
+    <h1>Hello</h1>
+  )
 }
 
 export default App;
