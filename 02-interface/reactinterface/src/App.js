8532922ad9a7d0471849342e1ee91b47f2b4568d
ralexrivero <ralexrivero@gmail.com>
2022-11-28 11:53:29 -0300

    pre tailwind installation

--- 02-interface/reactinterface/src/App.js
+++ 02-interface/reactinterface/src/App.js
@@ -1,9 +1,9 @@
-import { BiArchive } from "react-icons/bi";
+import { BiArchive } from 'react-icons/bi'
 
 function App() {
   return (
-    <div className="App container mx-auto mt-3 font-thin">
-      <h1 className="text-5xl text-red-400 align-top">< BiArchive className="inline-block" />Your Appointments!</h1>
+    <div className="App">
+      <h1 className="">< BiArchive className="" /> Your appointments</h1>
     </div>
   );
 }
