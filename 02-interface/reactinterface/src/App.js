08beb8c2bfd3b00aace742d40cb024fb72e5e1b0
ralexrivero <ralexrivero@gmail.com>
2022-11-28 15:02:38 -0300

    add new tags for testing

--- 02-interface/reactinterface/src/App.js
+++ 02-interface/reactinterface/src/App.js
@@ -5,6 +5,11 @@ function App() {
     <div className="App container mx-auto mt-3 font-thin">
       <h1 className="text-5xl">
         <BiArchive className="inline-block text-red-400 align-top" />Your Appointments</h1>
+        <ul className="list-disc list-inside">
+          <li>one</li>
+          <li>two</li>
+        </ul>
+        <p className="bg-red-800">Add Appointments</p>
     </div>
   );
 }
