f38b8d4e9c43f77c25dfeb24b17aca682bc947ad
ralexrivero <ralexrivero@gmail.com>
2022-11-28 01:25:38 -0300

    configure craco and tailwindcss

--- 02-interface/reactinterface/src/App.js
+++ 02-interface/reactinterface/src/App.js
@@ -1,10 +1,9 @@
-import { BiTrash } from "react-icons/bi";
+import { BiArchive } from "react-icons/bi";
 
 function App() {
   return (
-    <div className="App">
-      <h1>Your Appointments</h1>
-      < BiTrash />
+    <div className="App container mx-auto mt-3 font-thin">
+      <h1 className="text-5xl text-red-400 align-top">< BiArchive className="inline-block" />Your Appointments</h1>
     </div>
   );
 }
