eeb708024ed760d1369ee323d1f8b988512a15b4
ralexrivero <ralexrivero@gmail.com>
2022-12-01 10:59:49 -0300

    semistandard style fix and add Search component

--- 02-interface/reactinterface/src/App.js
+++ 02-interface/reactinterface/src/App.js
@@ -1,15 +1,9 @@
-import { BiArchive } from "react-icons/bi"
+import { BiArchive } from 'react-icons/bi';
 
-function App() {
+function App () {
   return (
-    <div className="App container mx-auto mt-3 font-thin">
-      <h1 className="text-5xl">
-        <BiArchive className="inline-block text-red-400 align-top" />Your Appointments</h1>
-        <ul className="list-disc list-inside">
-          <li>one</li>
-          <li>two</li>
-        </ul>
-        <p className="bg-red-800">Add Appointments</p>
+    <div className='App container mx-auto mt-3 font-thin'>
+      <h1 className='text-5xl'><BiArchive className='inline-block text-red-400 align-top' />Your Appointments</h1>
     </div>
   );
 }
