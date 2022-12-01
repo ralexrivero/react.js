092e6b81b7f49e6131eff041b08a2b9b2d6f40d4
ralexrivero <ralexrivero@gmail.com>
2022-12-01 15:31:58 -0300

    pass data to a component and semistandard fix

--- 02-interface/reactinterface/src/App.js
+++ 02-interface/reactinterface/src/App.js
@@ -1,6 +1,8 @@
 import { BiCalendar } from 'react-icons/bi';
 import Search from './components/Search';
 import AddAppointment from './components/AddAppointment';
+import AppointmentInfo from './components/AppointmentInfo';
+import appointmentList from './data.json';
 
 function App () {
   return (
@@ -8,6 +10,15 @@ function App () {
       <h1 className='text-5xl mb-3'><BiCalendar className='inline-block text-red-400 align-top' />Your Appointments</h1>
       <AddAppointment />
       <Search />
+      <ul className='divide-y divide-gray-200'>
+        {appointmentList
+          .map(appointment => (
+            <AppointmentInfo
+              key={appointment.id}
+              appointment={appointment}
+            />
+          ))}
+      </ul>
     </div>
   );
 }
