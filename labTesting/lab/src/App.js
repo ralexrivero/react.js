8807664e2ff7e63b3dc015df08d12343ecf50bfc
ralexrivero <ralexrivero@gmail.com>
2022-12-05 17:36:08 -0300

    create cancel button

--- labTesting/lab/src/App.js
+++ labTesting/lab/src/App.js
@@ -95,8 +95,13 @@ function App() {
           <input className='form-checkbox' type='checkbox' name='notificaciones' id='notificaciones' value='Si' defaultChecked></input>
         </div>
 
-        <div className='flex flex-wrap -mx-3 mb-6'>
-            <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline" type='submit'>Enviar</button>
+        <div className="flex flex-wrap -mx-3 mb-6">
+          <div className="w-full md:w-1/2 px-3 mb-6 md:mb-0">
+            <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline" type='submit'>Registrarse</button>
+          </div>
+          <div className="w-full md:w-1/2 px-3">
+            <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline" type='submit'>Cancelar</button>
+          </div>
         </div>
 
       </form>
